30dbaae62e7e55dd7cefff28f536df8a2fc2ed14
Janik9999 <44294754+Janik9999@users.noreply.github.com>
2022-06-01 16:16:54 +0200

    Update setupCamera.js

--- src/setupCamera.js
+++ src/setupCamera.js
@@ -7,7 +7,7 @@ async function setupCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({
         'audio': false,
         'video': {
-          facingMode: 'user',
+          facingMode: 'environment',
           height: {ideal:1920},
           width: {ideal: 1920},
         },
